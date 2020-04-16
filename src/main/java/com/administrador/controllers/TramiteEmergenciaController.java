d399a8d958d0cbe0921f16a11283d3e0df0775db
Willy Carpio <wfcg.only20@gmail.com>
2020-04-16 13:23:52 -0500

    adicionando tabla tramiteemergenciacabecera

--- src/main/java/com/administrador/controllers/TramiteEmergenciaController.java
+++ src/main/java/com/administrador/controllers/TramiteEmergenciaController.java
@@ -1,13 +1,20 @@
 package com.administrador.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.administrador.entities.TramiteEmergenciaEntity;
 import com.administrador.models.TramiteEmergenciaModel;
+import com.administrador.services.TramiteEmergenciaService;
 
 @Controller
 public class TramiteEmergenciaController {
+	
+	@Autowired
+    TramiteEmergenciaService tramiteEmergenciaService;
+	
 	@RequestMapping("/")
     public String enlaceIndex(Model model) {
         return "index";
@@ -18,6 +25,15 @@ public class TramiteEmergenciaController {
 	}
 	@RequestMapping("/cargaDocumentos")
     public String cargaDocumentos(Model model, TramiteEmergenciaModel tramiteEmergenciaModel) {
+		TramiteEmergenciaEntity tramiteEmergenciaEntityNuevo = new TramiteEmergenciaEntity();
+		tramiteEmergenciaEntityNuevo.setTipoDocumentoAseg(tramiteEmergenciaModel.getSelectTipoDocumento());
+		tramiteEmergenciaEntityNuevo.setNumeroDocumentoAseg(tramiteEmergenciaModel.getInputNroDocumento());
+		tramiteEmergenciaEntityNuevo.setApellidosNombresAseg(tramiteEmergenciaModel.getInputApellidosNombres());
+		tramiteEmergenciaEntityNuevo.setCelularAseg(tramiteEmergenciaModel.getInputCelular());
+		tramiteEmergenciaEntityNuevo.setCorreoAseg(tramiteEmergenciaModel.getInputCorreoElectronico());
+		tramiteEmergenciaEntityNuevo.setOficinaCEVITAseg(tramiteEmergenciaModel.getSelectOficinaCevit());
+		
+		tramiteEmergenciaService.addTramiteEmergencia(tramiteEmergenciaEntityNuevo);
 		
 		return "cargaDocumentos";
 	}
